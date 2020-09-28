bf0d6a4db84b0c2ff2e907d795b680e83368c6ad
radhakrushna <35287137+radhakrushna@users.noreply.github.com>
2020-09-28 21:51:29 +0530

    Update rexec.sh
    changes

--- .github/workflows/rexec.sh
+++ .github/workflows/rexec.sh
@@ -6,7 +6,6 @@ then
    echo "Remote command missing: Usage: \"rexec <remote command with paramaters>\""
    exit -1
 fi
-echo "HI--1"
 
 # Ideally should be set as an environment variable to reuse this script
 SSHUSER="radhakrushnahost"
@@ -16,35 +15,25 @@ REMOTE_IP="192.168.100.103"
 REMOTE_COMMAND="$*"
 # Clear known_hosts
 rm -f ~/.ssh/known_hosts
-echo "HI--2"
 command_with_sshpass="sshpass -p $SSHPASS ssh -o StrictHostKeychecking=no -n -l $SSHUSER $REMOTE_IP $REMOTE_COMMAND"
-echo "HI--3"
+echo "$command_with_sshpass"
 tmp_remote_error_file=$(mktemp)
 remote_output="`$command_with_sshpass 2>$tmp_remote_error_file`"
-echo "HI--4"
 remote_return_code=$?
-echo "HI--5"
 if [ $remote_return_code != 0 ] #  && $remote_output == "" ]
 then
-   echo "HI--6"
    remote_error=$(< $tmp_remote_error_file)
-   echo "HI--7"
    rm -rf $tmp_remote_error_file
-   echo "HI--8"
    if [ ! -z "$remote_error" ]
    then
       echo "$remote_error"
-      echo "HI--9"
       if [ ! -z "$remote_output" ]
       then
          echo "$remote_output"
-         echo "HI--10"
       fi
    fi
-   echo "HI--11"
    exit $remote_return_code
 fi
 
 echo "$remote_output"
-echo "HI--14"
 exit $remote_return_code
