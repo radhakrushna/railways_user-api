95c497c383b555f8ff48c72960742508e4d1e8c3
radhakrushna <35287137+radhakrushna@users.noreply.github.com>
2020-09-29 00:38:25 +0530

    Update rexec1.sh
    scp

--- .github/workflows/rexec1.sh
+++ .github/workflows/rexec1.sh
@@ -16,7 +16,13 @@ REMOTE_TYPE=$1
 REMOTE_COMMAND="$2"
 # Clear known_hosts
 rm -f ~/.ssh/known_hosts
-command_with_sshpass="sshpass -p $SSHPASS $REMOTE_TYPE -o StrictHostKeychecking=no -n -l $SSHUSER $REMOTE_IP $REMOTE_COMMAND"
+if [$REMOTE_TYPE == "ssh"]
+then
+   command_with_sshpass="sshpass -p $SSHPASS ssh -o StrictHostKeychecking=no -n -l $SSHUSER $REMOTE_IP $REMOTE_COMMAND"
+if else [$REMOTE_TYPE == "scp"]
+   command_with_sshpass="sshpass -p $SSHPASS scp -r /home/radha/Desktop/actions-runner/_work/railways_user-api/railways_user-api/katalonTest $SSHUSER@$REMOTE_IP:/home/radhakrushnahost/Desktop/"
+fi
+
 echo "$command_with_sshpass"
 tmp_remote_error_file=$(mktemp)
 remote_output="`$command_with_sshpass 2>$tmp_remote_error_file`"
